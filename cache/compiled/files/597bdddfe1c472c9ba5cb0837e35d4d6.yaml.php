68fa3358ecb290389500efa2f47fac7c185e2fbe
Marc Rimkus <mr@vonheldenundgestalten.de>
2019-05-20 21:19:22 +0200

    Del  cache add ignore

--- cache/compiled/files/597bdddfe1c472c9ba5cb0837e35d4d6.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/opt/lampp/htdocs/morhero.de/grav/user/themes/morhero/blueprints/projects.yaml',
-    'modified' => 1547192150,
-    'data' => [
-        'extends@' => 'page_base'
-    ]
-];
